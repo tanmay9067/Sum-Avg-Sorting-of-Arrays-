ca4d0e1c1a3b1972567675c160ebb974b4436426
Tanmay Argade <110284794+tanmay9067@users.noreply.github.com>
2024-09-23 22:34:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ages = {20, 22, 18, 35, 48, 26, 87, 70};
-        int sum = 0;
-        for(int i = 0 ; i < ages.length ; i++ ){
-            sum += ages[i];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input the length of array (1 to 20): ");
+        int n = scanner.nextInt();
+        while(n > 20 || n < 1){
+            System.out.println("Invalid Input");
+            n = scanner.nextInt();
         }
-        System.out.println("The sum of the all arrays is: " + sum);
-        double avg = (double)sum / (double) ages.length;
-        System.out.println("The average of the all arrays is: " + avg);
+        int [] numbers = new int[n];
+        //make function for purpose
+        addArrayToIndex(numbers);
+        showArrayFromListings(numbers);
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 
+    private static void showArrayFromListings(int[] numbers) {
+        System.out.print("The Solution is: ");
+    }
 
+    private static void addArrayToIndex(int[] numbers) {
+        Scanner input = new Scanner(System.in);
+        for(int i = 0 ; i < numbers.length ; i++){
+            numbers[i] = input.nextInt();
+        }
+    }
 }
-
-
